10f04c0157b22eebc5d91f01f5cbdc1d5acf801e
Sergey Afonin <afoninsb@yandex.ru>
2023-03-09 20:05:56 +0300

    aiohttp for file

--- utils/file.py
+++ utils/file.py
@@ -1,3 +1,5 @@
+import aiohttp
+import aiofiles
 import json
 import os
 from http import HTTPStatus
@@ -14,11 +16,16 @@ settings = Settings()
 async def get_file(data: dict[str, str]) -> dict[str, str] | None:
     """Получение информации о файле, отправленном юзером в бот."""
     method = f'{settings.API_URL}/getFile'
-    file = requests.post(method, data=data)
-    if file.status_code != HTTPStatus.OK:
-        raise FileNotGet('Файл не скачан с сервера Телеграм')
-    await print_log('info', 'Получена информация о файле с сервера Телеграм')
-    return json.loads(file._content)['result']
+    async with aiohttp.ClientSession() as session:
+        async with session.post(method, data=data) as resp:
+            response = await resp.json()
+        if resp.status != HTTPStatus.OK:
+            raise FileNotGet('Файл не скачан с сервера Телеграм')
+        await print_log(
+            'info',
+            'Получена информация о файле с сервера Телеграм'
+        )
+    return response['result']
 
 
 async def save_file(file_data: dict[str, str], chat_id: int) -> str | None:
@@ -28,14 +35,15 @@ async def save_file(file_data: dict[str, str], chat_id: int) -> str | None:
     path = file_data["file_path"]
     url = f'{settings.API_FILE_URL}/{path}'
     file_name = f'{chat_id}-{file_data["file_unique_id"]}.{path[-3:]}'
-    with open(f'{settings.DIR}/{file_name}', "wb") as f:
-        file = requests.get(url)
-        if file.status_code != HTTPStatus.OK:
-            raise FileNotGet('Файл не скачан с сервера Телеграм')
-        await print_log('info', 'Получен файл с серверов Телеграма')
-        try:
-            f.write(file.content)
-        except Exception as e:
-            raise FileNotSave('Файл не сохранен на диск') from e
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status != HTTPStatus.OK:
+                raise FileNotGet('Файл не скачан с сервера Телеграм')
+            try:
+                f = await aiofiles.open(f'{settings.DIR}/{file_name}', mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+            except Exception as e:
+                raise FileNotSave('Файл не сохранен на диск') from e
     await print_log('info', f'Файл сохранён на диск {file_name}')
     return file_name
